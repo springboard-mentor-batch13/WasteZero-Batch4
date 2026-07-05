094bc0ba1cefc87352bfe841914681c567bbdcbf
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2026-07-05 07:33:48 +0530

    add else before the token check

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -24,7 +24,7 @@ const protect = async (req,res,next)=>{
         }
     }
 
-    if(!token){
+    else if(!token){
         return res.status(401).json({ message: 'Not authorized — no token provided' });
     }
 }
