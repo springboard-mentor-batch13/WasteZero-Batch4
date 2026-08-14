6b6c29ba5ee1bd5970d95360917e573de58165fd
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2026-08-15 00:43:26 +0530

    fixing nodemailer default configuration

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,11 +2,16 @@ import nodemailer from 'nodemailer';
 
 const sendEmail = async ({ to, subject, text }) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
   });
 
   await transporter.sendMail({
